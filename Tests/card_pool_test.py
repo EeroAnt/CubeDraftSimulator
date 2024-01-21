b0a29b21d676ee46e65fa63df668d52426e3bed1
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-01-21 14:31:58 +0200

    setup first tests

---
+++ Tests/card_pool_test.py
@@ -0,0 +1,20 @@
+import unittest
+from Database_Handling.draft_setup import generate_pools
+
+class TestGeneratePools(unittest.TestCase):
+	def setUp(self):
+		self.pools = generate_pools(8)
+	
+	def test_amount_of_commanders(self):
+		self.assertEqual(len(self.pools["commanders"]), 40)
+	
+	def test_amount_of_multicolored_cards(self):
+		self.assertEqual(len(self.pools["multicolored_pool"]), 162)
+	
+	def test_that_no_commanders_are_in_multicolored_pool(self):
+		for commander in self.pools["commanders"]:
+			self.assertNotIn(commander, self.pools["multicolored_pool"])
+	
+	def test_amount_of_white_cards(self):
+		self.assertEqual(len(self.pools["white"]), 108)
+	
