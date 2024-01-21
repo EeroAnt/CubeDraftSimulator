83f672187be2caa55989d36309691915632ae752
Eero Antikainen <eeroantikainen90@gmail.com>
2024-01-21 23:07:59 +0200

    clean up

--- main.py
+++ main.py
@@ -6,9 +6,5 @@ commander_packs, normal_packs, conn = setup_draft(4)
 close_cloud_db(conn)
 
 print("Draft setup complete.")
-# for i in range(5):
-# 	print("Pack " + str(i+1) + ":")
-# 	for j in normal_packs[i]:
-# 		print(j["name"])
-# 		print(j["draft_pool"])
+
 	
