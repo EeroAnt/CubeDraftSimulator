6e2fcf3719cc74cc1d141918f848b9e46e3e9b47
Eero Antikainen <eeroantikainen90@gmail.com>
2024-01-21 20:24:40 +0200

    renamed folder

---
+++ Backend/draft_setup.py
@@ -0,0 +1,7 @@
+from Backend.pool_generation import generate_pools
+from Backend.packs import deal_commander_packs
+
+def setup_draft(player_count):
+	pools, conn = generate_pools(player_count)
+	commander_packs = deal_commander_packs(pools["commander"])
+	return pools, conn
--- Database_Handling/draft_setup.py
+++
@@ -1,5 +0,0 @@
-from Database_Handling.pool_generation import generate_pools
-
-def setup_draft(player_count):
-	pools, conn = generate_pools(player_count)
-	return pools, conn
