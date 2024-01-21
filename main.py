2df52c35bef946f282ec2351d408e3d1ed21468b
Eero Antikainen <eeroantikainen90@gmail.com>
2024-01-21 23:26:22 +0200

    add print for clocking the setup

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from Backend.draft_setup import setup_draft
 from Backend.cloud_db import close_cloud_db
-
+from time import time
+start = time()
 commander_packs, normal_packs, conn = setup_draft(4)
 
 close_cloud_db(conn)
 
 print("Draft setup complete.")
-
+print("Time elapsed: " + str(time() - start) + " seconds.")
 	
