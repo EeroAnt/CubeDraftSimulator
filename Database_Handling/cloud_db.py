706b94da155127c7e4ef402ee94e182cc337f07d
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-01-21 17:35:31 +0200

    populate cloud database

--- Database_Handling/cloud_db.py
+++ Database_Handling/cloud_db.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
-cnx = psycopg2.connect(user="cubeadmin", password=getenv("CLOUDDB-PASSWORD"), host="cube-simulator-db.postgres.database.azure.com", port=5432, database="cubedb")
-cur = cnx.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);")
-cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
-cnx.commit()
 
-cur.execute("SELECT * FROM test;")
-print(cur.fetchall())
+cnx = psycopg2.connect(
+	user="cubeadmin", 
+	password=getenv("CLOUDDB-PASSWORD"), 
+	host="cube-simulator-db.postgres.database.azure.com",
+	port=5432, 
+	database="cubedb")
+
+cur_cloud = cnx.cursor()
 
 cnx.close()
