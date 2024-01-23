363befd4e282c5c3e40f1cafd5fcccd5caf4abe5
Eero Antikainen <eeroantikainen90@gmail.com>
2024-01-23 16:44:19 +0200

    flask server actually works

--- Flask_server/src/routes/routes.py
+++ Flask_server/src/routes/routes.py
@@ -1,15 +1,27 @@
 from app import app
-from flask import render_template, request
+# from flask import request
+from flask_restful import Api, Resource
 from src.operations.setup_server import setup
 import json
 
-@app.route("/")
-def index():
-	return "Hello, World!"
+api = Api(app)
 
-@app.route("/setup/<string:identifier>", methods=["GET"])
-def test(identifier):
-	if request.method == "GET":
-		setup(4, identifier)
+class returnjson(Resource):
+	def get(self, player_count, identifier):
+		player_count = int(player_count)
+		setup(player_count, identifier)
 		data = json.load(open(f"templates/draft{identifier}.json"))
 		return data
+
+api.add_resource(returnjson, "/<player_count>/<identifier>")
+
+# @app.route("/")
+# def index():
+# 	return "Hello, World!"
+
+# @app.route("/setup/<string:identifier>", methods=["GET"])
+# def test(identifier):
+# 	if request.method == "GET":
+# 		setup(4, identifier)
+# 		data = json.load(open(f"templates/draft{identifier}.json"))
+# 		return data
