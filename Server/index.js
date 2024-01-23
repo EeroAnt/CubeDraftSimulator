deda75249c4a4910592f111f3407a4b4306a3750
Eero Antikainen <eeroantikainen90@gmail.com>
2024-01-23 17:30:33 +0200

    add api call to setup and get draft

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const app = express()
 const axios = require('axios')
-let data = []
 
-app.get('/api/draft', (request, response) => {
-  axios.get('http://127.0.0.1:5000/setup/trial534').then(res => {
-	console.log(res.data['pack0'])
-	data = res.data
+
+
+app.get('/api/init_draft/:player_count/:token', (request, response) => {
+  const player_count = request.params.player_count
+  const token = request.params.token
+  axios.get(`http://127.0.0.1:5000/${player_count}/${token}`).then(res => {
+  response.send("draft ready")
+  return res.data
+  }).catch(err => {
+	console.log(err)
+	console.log("Something went wrong with the draft setup")
   })
 })
-
-
+ 
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
