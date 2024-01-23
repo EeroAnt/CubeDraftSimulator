0c04ba23ecb0bb69117ca5acc307d6ea9add3481
Eero Antikainen <eeroantikainen90@gmail.com>
2024-01-23 20:11:00 +0200

    add DraftSetup.jsx and let .gitignore do so

---
+++ Simulator/src/Services/DraftSetup.jsx
@@ -0,0 +1,16 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/'
+
+const setupDraft = (id, players) => {
+  const SetupUrl = baseUrl + 'api/drafts/' + id +'/'+ players
+
+  return axios.get(SetupUrl
+	,{
+	headers: {
+	  'Access-Control-Allow-Origin': '*',
+  }
+}
+)
+}
+
+export default { setupDraft }
