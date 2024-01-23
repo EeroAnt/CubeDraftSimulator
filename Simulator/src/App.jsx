6ae159d843ac4b758e0d5afa46eee9035da4dcc6
Eero Antikainen <eeroantikainen90@gmail.com>
2024-01-23 20:10:03 +0200

    clean start for the web app

--- Simulator/src/App.jsx
+++ Simulator/src/App.jsx
@@ -1,34 +1,15 @@
 import { useState, useEffect } from 'react'
 import Form from './Components/Forms.jsx'
 import Buttons from './Components/Buttons.jsx'
+import DraftSetup from './Services/DraftSetup.jsx'
 import Packs from './Services/Packs.jsx'
 import Image from './Components/Image.jsx'
 import './App.css'
 
 
 function App() {
-  const [data, setData] = useState(null)
   const [mode, setMode] = useState(0)
   const [numberOfPlayers, setNumberOfPlayers] = useState(4)
-  const [draftSetup, setDraftSetup] = useState(false)
-  const [packs, setPacks] = useState([])
-  const [draftOnline, setDraftOnline] = useState(false)
-  const [packsInRound, setPacksInRound] = useState([])
-  const [cardsAtHand, setCardsAtHand] = useState([])
-  const [playerNumber, setPlayerNumber] = useState(0)
-  const [chosenCard, setChosenCard] = useState("") 
-  const [mainDeck, setMainDeck] = useState([])
-  const [pickNumber, setPickNumber] = useState(0)
-  const [cardPicked, setCardPicked] = useState(false)
-  const [roundNumber, setRoundNumber] = useState(0)
-  const [playerQueue, setPlayerQueue] = useState([])
-  const [players, setPlayers] = useState([])
-
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
 
   const changePlayerNumber = (event) => {
 	event.preventDefault()
@@ -37,15 +18,11 @@ function App() {
 
   const setupDraft = (event) => {
 	event.preventDefault()
-	console.log(data)
 	setMode(1)
-	setDraftSetup(true)
-	// setPlayerQueue(new Array(numberOfPlayers).fill([]))
-	// setPlayers(new Array(numberOfPlayers-1).fill().concat({PlayerObject(name="human", ishuman=true, id=0)}))
-	Packs.getAll()
-	.then(response=> {
-	  setPacks(response.data)
-	})
+	var token = function() {
+		return Math.random().toString(36).slice(2,6)
+	}
+	DraftSetup.setupDraft(numberOfPlayers, token())
   }
 
   const cancelSetup = (event) => {
@@ -57,39 +34,14 @@ function App() {
   const initDraft = (event) => {
 	event.preventDefault()
 	setMode(2)
-	setPacksInRound(packs[roundNumber])
-	setCardsAtHand(packs[roundNumber][playerNumber])
   }
 
-  const pickCard = (event) => {
-	event.preventDefault()
-	if (chosenCard === "") {
-	  return
-	}
-	else {
-	const card = cardsAtHand.find(card => card.id === chosenCard)
-	let newPacks = packsInRound
-	newPacks[(playerNumber+pickNumber)%numberOfPlayers] = packsInRound[(playerNumber+pickNumber)%numberOfPlayers].filter(card => card.id !== chosenCard)
-	setPacksInRound(newPacks)
-	setMainDeck(mainDeck.concat(card))
-	setPickNumber(pickNumber + 1)
-	setChosenCard("")
-	setCardPicked(true)
-	}
-  }
-
-  const nextPack = (event) => {
-	event.preventDefault()
-	setCardsAtHand(packsInRound[(playerNumber + pickNumber) % numberOfPlayers])
-	console.log(mainDeck)
-	setCardPicked(false)
-	  }
-
+  
 
   if (mode === 0) {
   return (
     <>
-	  <h1>Simulator</h1>
+	  <h1>Let's draft cube!</h1>
 	  <Form.Dropdown name="number of players" handleChange={changePlayerNumber}/>
 	  <Buttons.Button name="init draft" onClick={setupDraft}/>
 	</>
@@ -98,7 +50,7 @@ function App() {
   else if (mode === 1){
 	return (
 	  <>
-	    <h1>Simulator</h1>
+	    <h1>Let's draft cube!</h1>
 		<h3>Number of players = {numberOfPlayers}</h3>
 		<Buttons.Button name="start draft" onClick={initDraft}/>
 		<Buttons.Button name="cancel" onClick={cancelSetup}/>
@@ -107,16 +59,10 @@ function App() {
   }
   else {
 	return (  
-	<>{cardPicked ? "" :
-	  <tr>
-		{cardsAtHand.map(
-		  card => <td key={card.id}
-		    className={chosenCard === card.id ? "Selected" : "table-cell" }
-			onClick={() => (setChosenCard(card.id))}>
-		      <Image imageUrl={card.image_url} backsideUrl={card.backside_image_url} />
-		</td>)}
-	  </tr>}
-	  <Buttons.Button name={cardPicked ? "Next pack" : "Confirm pick"} onClick={cardPicked ? nextPack :pickCard }/>
+	<>
+	  <h1>Under construction</h1>
+	  <h2>Let's not draft cube yet</h2>
+	  <Buttons.Button name="Go Back" onClick={cancelSetup}/>
 	</>
   )
 }
