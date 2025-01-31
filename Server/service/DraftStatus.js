75f8001ef348755786ab64c7e14ddfa19a39217e
Eero Antikainen <eeroantikainen90@gmail.com>
2025-01-31 10:43:20 +0200

    refactor goToNextRound out of the main function

--- Server/service/DraftStatus.js
+++ Server/service/DraftStatus.js
@@ -4,71 +4,79 @@ import { users, connections, intervalIDs } from "./State.js";
 
 export function checkDraftStatus(draft) {
   if (draft.state === 'drafting' && draft.players.length > 0) {
-	if (checkIfRoundIsDone(draft.table)) {
-	  draft.round ++;
-	  if (draft.round < Object.keys(draft.rounds).length) {
-		console.log('round:',draft.round);
-		draft.direction *= -1;
-		for (let i = 0; i < draft.player_count; i++) {
-		  const player = draft.players[i];
-		  const pack = [draft.rounds[draft.round][`pack${i}`]];
-		  
-      const playerOnTheLeft =
-        users[draft.table[`seat${[calculateNextSeatNumber(
-          player.seat.number,
-          1,
-          draft.player_count)]}`].player];
-		  
-      const playerOnTheRight =
-        users[draft.table[`seat${[calculateNextSeatNumber(
-          player.seat.number,
-          -1,
-          draft.player_count
-        )]}`].player];
-      
-		  connections[draft.players[i].uuid].send(JSON.stringify({
-        status: "OK",
-        type: "Neighbours",
-        left: playerOnTheLeft.username,
-        right: playerOnTheRight.username,
-        direction: draft.direction,
-        seatToken: player.seat.token
-      }));
+    if (checkIfRoundIsDone(draft.table)) {
+      draft.round++;
+      if (draft.round < Object.keys(draft.rounds).length) {
+        goToNextRound(draft);
+      } else {
+        draft.state = 'deckbuilding';
+        broadcastDraftStatus(draft, "Post Draft");
+        clearInterval(intervalIDs[draft.token]);
+      }
+    } else {
+      for (const seat in draft.table) {
+        if (draft.table[seat].packAtHand.cards.length === 0 &&
+          draft.table[seat].queue.length > 0) {
 
-		  player.seat.queue = pack;
-	    }} else {
-		draft.state = 'deckbuilding';
-		broadcastDraftStatus(draft, "Post Draft");
-		clearInterval(intervalIDs[draft.token]);
-		}
-	} else {
-	  for (const seat in draft.table) {
-		if (draft.table[seat].packAtHand.cards.length === 0 &&
-      draft.table[seat].queue.length > 0) {
+          console.log('giving pack to player');
 
-		  console.log('giving pack to player');
-
-		  draft.table[seat].packAtHand = draft.table[seat].queue.shift();
-		  connections[draft.table[seat].player].send(JSON.stringify({
-        status: "OK",
-        type: "Pack",
-        pack: draft.table[seat].packAtHand.cards
-      }));
-		}
-	  }
-	}
-  } else if (draft.players.length === 0 && draft.state === 'drafting'){
-  	draft.state = 'disconnected';
-  	console.log('disconnected');
+          draft.table[seat].packAtHand = draft.table[seat].queue.shift();
+          connections[draft.table[seat].player].send(JSON.stringify({
+            status: "OK",
+            type: "Pack",
+            pack: draft.table[seat].packAtHand.cards
+          }));
+        }
+      }
+    }
+  } else if (draft.players.length === 0 && draft.state === 'drafting') {
+    draft.state = 'disconnected';
+    console.log('disconnected');
 
   } else if (draft.state === 'done') {
-  	console.log('draft ended');
-	  clearInterval(intervalIDs[draft.token]);
-	  broadcastDraftStatus(draft, "End Draft");
+    console.log('draft ended');
+    clearInterval(intervalIDs[draft.token]);
+    broadcastDraftStatus(draft, "End Draft");
   } else if (draft.state === 'deckbuilding') {
-	
+
   } else {
-  	clearInterval(intervalIDs[draft.token]);
-	  console.log('draft ended');
+    clearInterval(intervalIDs[draft.token]);
+    console.log('draft ended');
+  }
+}
+
+
+function goToNextRound(draft) {
+  console.log('round:', draft.round);
+  draft.direction *= -1;
+  for (let i = 0; i < draft.player_count; i++) {
+    const player = draft.players[i];
+    const pack = [draft.rounds[draft.round][`pack${i}`]];
+
+    const playerOnTheLeft =
+      users[draft.table[`seat${[calculateNextSeatNumber(
+        player.seat.number,
+        1,
+        draft.player_count)]}`].player];
+
+    const playerOnTheRight =
+      users[draft.table[`seat${[calculateNextSeatNumber(
+        player.seat.number,
+        -1,
+        draft.player_count
+      )]}`].player];
+
+    const message = {
+      status: "OK",
+      type: "Neighbours",
+      left: playerOnTheLeft.username,
+      right: playerOnTheRight.username,
+      direction: draft.direction,
+      seatToken: player.seat.token
+    };
+
+    connections[draft.players[i].uuid].send(JSON.stringify(message));
+
+    player.seat.queue = pack;
   }
 }
