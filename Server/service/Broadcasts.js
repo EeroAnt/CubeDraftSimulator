7af507c0d5704192f51d6a1cf2561284ab67ba53
Eero Antikainen <eeroantikainen90@gmail.com>
2025-01-31 11:24:00 +0200

    refactor broadcasts to use messaging module

--- Server/service/Broadcasts.js
+++ Server/service/Broadcasts.js
@@ -1,4 +1,5 @@
 import { connections, users } from "./State.js";
+import { sendMessage } from "./Messaging.js";
 
 export const broadcastUserlist = (draft) => {
   Object.values(draft.players).forEach(player => {
@@ -7,9 +8,11 @@ export const broadcastUserlist = (draft) => {
 		return acc;
 	  }, { status: "OK", type: "Playerlist", players: {} });
 
-	const message = JSON.stringify(result);
+	const message = result;
 	if (Object.keys(connections).includes(player.uuid)) {
-	    connections[player.uuid].send(message);
+      
+    sendMessage(player.uuid, message);
+    
     }
  });
 };
@@ -17,8 +20,10 @@ export const broadcastUserlist = (draft) => {
 
 export const broadcastDraftStatus = (draft, status) => {
   Object.values(draft.players).forEach(player => {
-	  const message = JSON.stringify({ status: "OK", type: status });
-    connections[player.uuid].send(message);
+	  const message = { status: "OK", type: status };
+    
+    sendMessage(player.uuid, message);
+
   });
 };
 
@@ -26,33 +31,37 @@ export const broadcastDraftStatus = (draft, status) => {
 export const broadcastCanalDredger = (draft, seatNumber) => {
   Object.values(draft.players).forEach(player => {
 	if (player.seat.number === seatNumber) {
-	  const message = JSON.stringify({
+	  const message = {
       status: "OK",
       type: "Canal Dredger",
       seat: seatNumber,
       owner: true
-    });
-	  
-    connections[player.uuid].send(message);
+    };
+
+	  sendMessage(player.uuid, message);
+
 	} else {
-  	const message = JSON.stringify({
+  	const message = {
       status: "OK",
       type: "Canal Dredger",
       seat: seatNumber,
       owner: false
-    });
+    };
 
-	  connections[player.uuid].send(message);
+    sendMessage(player.uuid, message);
+
   }});
 };
 
 export function sendCards(uuid) {
-  const message = JSON.stringify({
+  const message = {
     status: "OK",
     type: "Picked Cards",
     commanders: users[uuid].seat.commanders,
     main: users[uuid].seat.main,
     side: users[uuid].seat.side
-  });
-  connections[uuid].send(message);
+  };
+
+  sendMessage(uuid, message);
+  
 }
