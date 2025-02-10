520e7af63bb8cb7bd041a48126384234b8bfa009
Eero Antikainen <eeroantikainen90@gmail.com>
2025-02-10 12:12:46 +0200

    init eslint and prettier

--- CubeStats/CubeStatsFront/src/App.jsx
+++ CubeStats/CubeStatsFront/src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Welcome to Eero&apos;s Cube Stats</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
