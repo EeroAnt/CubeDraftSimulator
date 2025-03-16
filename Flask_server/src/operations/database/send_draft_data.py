9a7e76334ce6d711114bd94a780824f4d999718b
Eero Antikainen <eeroantikainen90@gmail.com>
2025-03-16 08:48:31 +0200

    change flask server to support refactored collection of draft data

--- Flask_server/src/operations/database/send_draft_data.py
+++ Flask_server/src/operations/database/send_draft_data.py
@@ -1,23 +1,29 @@
 from src.operations.database.db import connect_to_db
-from src.operations.database.queries import sending_packs_query, sending_commander_packs_query
-from json import dumps
+from src.operations.database.queries.send_draft_data_queries import sending_packs_query, sending_commander_packs_query, sending_draft_query
+from json import load
 
 def send_draft_data(data):
-  with open ("data.json", "w") as f:
-    f.write(dumps(data))
-  # cur, conn = connect_to_db()
-  # cur.execute("BEGIN;")
+  # This is for testing purposes
+  # with open ("data.json", "r") as f:
+  #   data = load(f)
+  cur, conn = connect_to_db()
+  cur.execute("BEGIN;")
 
-  # commander_packs = list(filter(lambda x: len(x)==5, data["packs"]))
-  # normal_packs = list(filter(lambda x: len(x)!=5, data["packs"]))
+  commander_packs = list(filter(lambda x: len(x)==5, data["packs"]))
+  normal_packs = list(filter(lambda x: len(x)!=5, data["packs"]))
 
-  # for pack in normal_packs:
-  #   cur.execute(sending_packs_query(), (tuple(pack)))
+  for pack in normal_packs:
+    cur.execute(sending_packs_query(), (tuple(pack)))
 
-  # for pack in commander_packs:
-  #   cur.execute(sending_commander_packs_query(), (tuple(pack)))
+  for pack in commander_packs:
+    cur.execute(sending_commander_packs_query(), (tuple(pack)))
 
-  # cur.execute("COMMIT;")
-  # conn.close()
+  for i in data["pools"]:
+    for card in i["cards"]:
+      cur.execute(sending_draft_query(), (i["draftToken"], card, i["seatToken"], i["username"]))
+
+  cur.execute("COMMIT;")
+  conn.close()
 
   return "success"
+
