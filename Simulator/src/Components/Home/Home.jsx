4dd494a6c9d353c77beab990e2d004040095ff75
Eero Antikainen <eeroantikainen90@gmail.com>
2025-03-18 15:43:25 +0200

    delete commented out code

--- Simulator/src/Components/Home/Home.jsx
+++ Simulator/src/Components/Home/Home.jsx
@@ -61,29 +61,4 @@ export const Home = ({
       )}
     </div>
   )
-  //   <div className="main">
-  //     <HomeNavBar />
-  //     <h1>Hi {username}</h1>
-  //     {draftInitiated ? (
-  //       <h2>Waiting for response</h2>
-  //     ) : (
-  //       <>
-  // <h2>Setup a new Draft</h2>
-  // <DraftParametersForm name="number of players" handleChange={(e) => { e.preventDefault(); setNumberOfPlayers(Number(e.target.value)) }} defaultVal={numberOfPlayers} />
-  // <DraftParametersForm name="number of rounds" handleChange={(e) => { e.preventDefault(); setNumOfRounds(Number(e.target.value)) }} defaultVal={numOfRounds} />
-  // <DraftParametersForm name="ratio of multi color pool" handleChange={(e) => { e.preventDefault(); setMultiRatio(Number(e.target.value)) }} defaultVal={multiRatio} />
-  // <DraftParametersForm name="ratio of generic pool" handleChange={(e) => { e.preventDefault(); setGenericRatio(Number(e.target.value)) }} defaultVal={genericRatio} />
-  // <DraftParametersForm name="ratio of colorless pool" handleChange={(e) => { e.preventDefault(); setColorlessRatio(Number(e.target.value)) }} defaultVal={colorlessRatio} />
-  // <DraftParametersForm name="ratio of land pool" handleChange={(e) => { e.preventDefault(); setLandRatio(Number(e.target.value)) }} defaultVal={landRatio} />
-  // <DraftParameterCheckbox name="Commander pack included" handleChange={changeCommanderPacksIncluded} />
-
-  // <Button name="init draft" onClick={() => submitSetup()} />
-
-  //         <h2>Join Draft with a token</h2>
-
-  //         <Form onSubmit={joinLobby} name="joindraft" />
-  //       </>
-  //     )}
-  //   </div>
-  // )
 }
