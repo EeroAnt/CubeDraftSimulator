114e7f52fc409082d749a4483079ad14e3039379
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-02 21:50:16 +0300

    add loading icon and move data fetching here

--- CubeStats/CubeStatsFront/src/App.jsx
+++ CubeStats/CubeStatsFront/src/App.jsx
@@ -1,14 +1,33 @@
 import "./App.css";
 import { NavBar } from "/src/components/NavBar/NavBar";
 import { Overview } from "./components/Views/Overview/Overview";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [showDraftPools, setShowDraftPools] = useState(false);
   const [showColorIdMenu, setShowColorIdMenu] = useState(false);
   const [colorIdState, setColorIdState] = useState("");
   const [draftPoolsState, setDraftPoolsState] = useState("");
   const [mode, setMode] = useState("home");
+  const [data, setData] = useState([]);
+
+  const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect(() => {
+    fetch(baseUrl + "/api/data")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
+  useEffect(() => {
+    if (data.single_color_avg_picks) {
+      setLoading(false);
+    }
+  }, [data]);
+
   return (
     <>
       <NavBar
@@ -19,8 +38,13 @@ function App() {
         setColorIdState={setColorIdState}
         setDraftPoolState={setDraftPoolsState}
         setMode={setMode}
+        data={data}
       />
-      {mode === "home" ? <Overview /> : null}
+      {loading ?
+        <div className="mt-10 flex items-center justify-center">
+          <div className="w-10 h-10 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+        </div> : null}
+      {mode === "home" && !loading ? <Overview data={data} /> : null}
     </>
   );
 }
