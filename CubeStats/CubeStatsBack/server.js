9e21afb313a2bd963d03ce93a54391ad337d0f9e
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-02 20:07:25 +0300

    successfully test connection to cached data

--- CubeStats/CubeStatsBack/server.js
+++ CubeStats/CubeStatsBack/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 
+dotenv.config();
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -11,8 +14,18 @@ app.get("/api/hello", (req, res) => {
   res.json({ message: "Yhteys toimii!" });
 });
 
-app.get("/api/data", (req, res) => {
-  res.json({ message: "Data toimii!" });
+app.get("/api/data", async (req, res) => {
+
+  try {
+    const response = await fetch(process.env.DATA_URL);
+    const data = await response.json();
+    console.log(data);
+    console.log(typeof data);
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- CubeStats/CubeStatsFront/src/components/Views/Overview/Overview.jsx
+++ CubeStats/CubeStatsFront/src/components/Views/Overview/Overview.jsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from "react";
 
 export const Overview = () => {
   const [message, setMessage] = useState("");
+  const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/data")
+    fetch(baseUrl + "/api/data")
       .then((res) => res.json())
-      .then((data) => setMessage(data.message))
+      .then((data) => setMessage(data))
       .catch((err) => console.error("Virhe haettaessa dataa:", err));
   }, []);
 
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center">
       <h1 className="text-4xl font-bold text-blue-600">Overview</h1>
-      <p>{message}</p>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4" onClick={() => console.log(message)}>
+        Click Me
+      </button>
     </div>
   );
 };
