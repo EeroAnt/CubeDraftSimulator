73cbb46c56e5c8ec33db4de67f4f6f27d119c859
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-04 22:37:03 +0300

    finalize initial data cache service and create initial full data

--- CubeStats/CubeStatsData/operations/data_cache_generator.py
+++ CubeStats/CubeStatsData/operations/data_cache_generator.py
@@ -1,21 +1,12 @@
 from operations.json_writer import write_json
-from operations.get_cards import generate_cards_object
+from operations.card_data import generate_cards_object
 from operations.temp_tables_creator import create_temp_tables
+from operations.aggragates import get_aggregates
 
 def generateDataCache(cursor):
-  # Run the SQL queries to generate the data cache
-  create_temp_tables(cursor)
-  # Store the results in a dictionary
-  # Write the json file
   data = {}
-  # Example data
-  data["single_color_avg_picks"] = {
-    "R": 6,
-    "G": 7,
-    "B": 8,
-    "W": 6.6,
-    "U": 7.2
-  }
+  create_temp_tables(cursor)
+  data["aggragates"] = get_aggregates(cursor)
   data["cards"] = generate_cards_object(cursor)
   write_json(data)
   return
