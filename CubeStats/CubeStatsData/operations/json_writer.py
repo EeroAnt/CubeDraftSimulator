4c1b704d0024e059c54161acf3d86f134f1fbe98
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-04 22:14:16 +0300

    add decoder to handle decimal values

--- CubeStats/CubeStatsData/operations/json_writer.py
+++ CubeStats/CubeStatsData/operations/json_writer.py
@@ -1,5 +1,6 @@
 from json import dump
+from utils.utils import DecimalEncoder
 
 def write_json(data):
   with open("data.json", "w") as f:
-    dump(data, f, indent=2)
+    dump(data, f, indent=2, cls=DecimalEncoder)
