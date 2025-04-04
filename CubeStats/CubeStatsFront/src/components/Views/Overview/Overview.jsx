b6e1d760ac667ae8ccc194fddf8314a9e88f8f6a
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-04 23:10:01 +0300

    use actual data

--- CubeStats/CubeStatsFront/src/components/Views/Overview/Overview.jsx
+++ CubeStats/CubeStatsFront/src/components/Views/Overview/Overview.jsx
@@ -1,25 +1,36 @@
 import { Chart } from '../../Charts'
 
 export const Overview = ({ data }) => {
-
+  const singleColorAverages = data?.aggragates?.color_id_averages?.filter(
+    (color) => color[0].length === 1
+  );
+  const twoColorAverages = data?.aggragates?.color_id_averages?.filter(
+    (color) => color[0].length === 2
+  );
+  const threeColorAverages = data?.aggragates?.color_id_averages?.filter(
+    (color) => color[0].length === 3
+  );
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center">
       <h1 className="text-4xl font-bold text-blue-600 mt-6">Overview</h1>
+      <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition duration-300" onClick={() => console.log(singleColorAverages)}>
+        <>Back to Cube</>
+      </button>
       {data && (
         <>
           <div className="w-full max-w-4xl p-6 bg-white shadow-md rounded-lg mt-6">
             <h2 className="text-2xl font-semibold mb-4">Single Color Average Picks</h2>
-            <Chart data={data.single_color_avg_picks} />
+            <Chart data={singleColorAverages} />
           </div>
 
           <div className="w-full max-w-4xl p-6 bg-white shadow-md rounded-lg mt-6">
             <h2 className="text-2xl font-semibold mb-4">Two Color Average Picks</h2>
-            <Chart data={data.single_color_avg_picks} />
+            <Chart data={twoColorAverages} />
           </div>
 
           <div className="w-full max-w-4xl p-6 bg-white shadow-md rounded-lg mt-6">
             <h2 className="text-2xl font-semibold mb-4">Three Color Average Picks</h2>
-            <Chart data={data.single_color_avg_picks} />
+            <Chart data={threeColorAverages} />
           </div>
         </>
       )}
