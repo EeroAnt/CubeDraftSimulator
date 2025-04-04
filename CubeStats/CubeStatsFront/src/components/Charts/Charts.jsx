df14156537338c769969430310557ffd47d05611
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-04 23:09:39 +0300

    handle decimals and lists

--- CubeStats/CubeStatsFront/src/components/Charts/Charts.jsx
+++ CubeStats/CubeStatsFront/src/components/Charts/Charts.jsx
@@ -8,9 +8,9 @@ import {
 } from 'recharts';
 
 export function Chart({ data }) {
-  const chartData = Object.entries(data).map(([key, value]) => ({
-    name: key, // 'R', 'G', 'B', etc.
-    value: value, // 6, 7, 8, etc.
+  const chartData = data.map(([name, value]) => ({
+    name: name,
+    value: value,
   }));
   const maxValue = Math.max(...chartData.map(item => item.value));
   const minValue = Math.min(...chartData.map(item => item.value));
@@ -18,8 +18,11 @@ export function Chart({ data }) {
     <ResponsiveContainer width="100%" height={300}>
       <BarChart data={chartData}>
         <XAxis dataKey="name" />
-        <YAxis domain={[minValue - 1, maxValue + 1]} />
-        <Tooltip />
+        <YAxis
+          domain={[Math.floor(minValue - 1), Math.ceil(maxValue + 1)]}
+          tickCount={7}
+        />
+        <Tooltip formatter={(value) => [Number(value).toFixed(2), "Value"]} />
         <Bar dataKey="value" fill="#8884d8" />
       </BarChart>
     </ResponsiveContainer>
