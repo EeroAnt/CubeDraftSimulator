16a89069463073fd3eaa9b7ac66c9cdbf5e286b6
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-04 21:59:53 +0300

    rewrite most of the functions and use static queries

--- CubeStats/CubeStatsData/operations/get_cards.py
+++ CubeStats/CubeStatsData/operations/get_cards.py
@@ -1,15 +1,41 @@
 from operations.queries import *
 
 def generate_cards_object(cursor):
+    cards = get_cards(cursor)
     commander_ids = get_commander_ids(cursor)
-    cursor.execute(random_test_cards_query(50))
+    
     pick_rates = get_pick_rates(cursor)
     commander_pick_rates = get_commander_pick_rates(cursor)
-    rows = cursor.fetchall()
+
     picked_cards = []
     cards_not_picked = []
-    for row in rows:
-        card = {
+    for card in cards:
+
+        if card["id"] in commander_ids:
+            card["is_commander"] = "true"
+        if card["id"] in pick_rates:
+            card["avg_pick"] = pick_rates[card["id"]]["avg_pick"]
+            card["amount_of_picks"] = pick_rates[card["id"]]["amount_of_picks"]
+        if card["id"] in commander_pick_rates:
+            card["avg_commander_pick"] = commander_pick_rates[card["id"]]["avg_pick"]
+            card["amount_of_commander_picks"] = commander_pick_rates[card["id"]]["amount_of_picks"]
+        if card["id"] in pick_rates or card["id"] in commander_pick_rates:
+            picked_cards.append(card)
+        else:
+            cards_not_picked.append(card)
+
+    cards_data = {
+        "picked_cards": picked_cards,
+        "cards_not_picked": cards_not_picked
+    }
+
+    return cards_data
+
+def get_cards(cursor):
+    cursor.execute(GET_CARDS_QUERY)
+    rows = cursor.fetchall()
+    return [
+        {
             "id": row[0],
             "name": row[1],
             "mv": row[2],
@@ -20,53 +46,35 @@ def generate_cards_object(cursor):
             "backside_image_url": row[7],
             "draft_pool": row[8]
         }
-
-        if row[0] in commander_ids:
-            card["is_commander"] = "true"
-        if row[0] in pick_rates:
-            card["avg_pick"] = pick_rates[row[0]]["avg_pick"]
-            card["amount_of_picks"] = pick_rates[row[0]]["amount_of_picks"]
-        if row[0] in commander_pick_rates:
-            card["avg_commander_pick"] = commander_pick_rates[row[0]]["avg_pick"]
-            card["amount_of_commander_picks"] = commander_pick_rates[row[0]]["amount_of_picks"]
-        if row[0] in pick_rates or row[0] in commander_pick_rates:
-            picked_cards.append(card)
-        else:
-            cards_not_picked.append(card)
-
-    cards = {
-        "picked_cards": picked_cards,
-        "cards_not_picked": cards_not_picked
-    }
-
-    return cards
+        for row in rows
+    ]
 
 def get_commander_ids(cursor):
-    cursor.execute(get_commander_ids_query())
+    cursor.execute(GET_COMMANDER_IDS_QUERY)
     rows = cursor.fetchall()
-    commander_ids = []
-    for row in rows:
-        commander_ids.append(row[0])
-    return commander_ids
+    return [
+        row[0]
+        for row in rows
+    ]
 
 def get_pick_rates(cursor):
-    cursor.execute(get_pick_rates_query())
+    cursor.execute(GET_PICK_RATES_QUERY)
     rows = cursor.fetchall()
-    pick_rates = {}
-    for row in rows:
-        pick_rates[row[0]] = {
+    return {
+        row[0]: {
             "avg_pick": row[1],
             "amount_of_picks": row[2]
         }
-    return pick_rates
+        for row in rows
+    }
 
 def get_commander_pick_rates(cursor):
-    cursor.execute(get_commander_pick_rates_query())
+    cursor.execute(GET_COMMANDER_PICK_RATES_QUERY)
     rows = cursor.fetchall()
-    commander_pick_rates = {}
-    for row in rows:
-        commander_pick_rates[row[0]] = {
+    return {
+        row[0]: {
             "avg_pick": row[1],
             "amount_of_picks": row[2]
         }
-    return commander_pick_rates
+        for row in rows
+    }
