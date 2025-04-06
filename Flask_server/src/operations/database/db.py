43150380c4d91fce95b98b5a88869074e6f977ab
Eero Antikainen <eero.antikainen@helsinki.fi>
2025-04-06 12:37:47 +0300

    finishing debugging and add some logs

--- Flask_server/src/operations/database/db.py
+++ Flask_server/src/operations/database/db.py
@@ -8,18 +8,15 @@ from os import getenv
 load_dotenv()
 
 def connect_to_db():
+  print("Connecting to DB...")
 
   key_str = getenv("SSH_KEY")
-  passphrase = getenv("SSH_PASSPHRASE", None)
-  private_key = RSAKey.from_private_key(
-    StringIO(key_str),
-    password=passphrase,
-    ssh_missing_host_key_policy=MissingHostKeyPolicy()
-    )
+  private_key = RSAKey.from_private_key(StringIO(key_str))
   server = SSHTunnelForwarder(
     (getenv("DNS"), int(getenv("DNS_PORT"))),
     ssh_username=getenv("SSH_USER"),
     ssh_pkey=private_key, 
+    ssh_password=getenv("SSH_PASSPHRASE"),
     remote_bind_address=('127.0.0.1', int(getenv("DB_PORT"))),
     local_bind_address=('0.0.0.0', 6543),
     set_keepalive=30,
@@ -40,5 +37,6 @@ def connect_to_db():
   return cur, conn, server
 
 def close_db(cnx, server):
+  print("Closing DB connection...")
   cnx.close()
   server.stop()
