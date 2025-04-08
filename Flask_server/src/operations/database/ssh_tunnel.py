c5eade06803b2a46f285cd3bbef2c88bdfd79422
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-08 20:59:42 +0300

    add retries and delay to ssh tunnel to be more reliable

--- Flask_server/src/operations/database/ssh_tunnel.py
+++ Flask_server/src/operations/database/ssh_tunnel.py
@@ -1,10 +1,11 @@
-from sshtunnel import SSHTunnelForwarder
+from sshtunnel import SSHTunnelForwarder, BaseSSHTunnelForwarderError
 from paramiko import RSAKey
 from io import StringIO
 from dotenv import load_dotenv
 from os import getenv
+from time import sleep
 
-def create_tunnel():
+def create_tunnel(retries=3, delay=5):
   
   load_dotenv()
 
@@ -14,19 +15,27 @@ def create_tunnel():
     StringIO(key_str),
     password=passphrase
     )
-  server = SSHTunnelForwarder(
-    (getenv("DNS"), int(getenv("DNS_PORT"))),
-    ssh_username=getenv("SSH_USER"),
-    ssh_pkey=private_key, 
-    ssh_password=getenv("SSH_PASSPHRASE"),
-    remote_bind_address=('127.0.0.1', int(getenv("DB_PORT"))),
-    local_bind_address=('0.0.0.0', 6543),
-    set_keepalive=30,
-  )
-
-  server.start()
-  print("Tunnel created.")
-  return server
+  for attempt in range(retries):
+    print(f"Attempting to create SSH tunnel... (Attempt {attempt+1}/{retries})")
+    try:
+      server = SSHTunnelForwarder(
+        (getenv("DNS"), int(getenv("DNS_PORT"))),
+        ssh_username=getenv("SSH_USER"),
+        ssh_pkey=private_key, 
+        ssh_password=getenv("SSH_PASSPHRASE"),
+        remote_bind_address=('127.0.0.1', int(getenv("DB_PORT"))),
+        local_bind_address=('0.0.0.0', 6543),
+        set_keepalive=30,
+      )
+      server.start()
+      print("Tunnel created.")
+      return server
+    except BaseSSHTunnelForwarderError as e:
+              print(f"SSH tunnel attempt {attempt+1} failed: {e}")
+              if attempt < retries - 1:
+                  sleep(delay)
+              else:
+                  raise
 
 def close_tunnel(server):
   print("Tunnel closed.")
