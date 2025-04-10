307d57a6d94d0025e028217b5dbe5163451c08c2
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-10 11:48:57 +0300

    add color id filter buttons to multi draft pool view

--- CubeStats/CubeStatsFront/src/components/Views/DraftPool/DraftPool.jsx
+++ CubeStats/CubeStatsFront/src/components/Views/DraftPool/DraftPool.jsx
@@ -1,4 +1,4 @@
-import { TextFilter, TwoThumbSlider, DraftedCardView } from "../../";
+import { TextFilter, TwoThumbSlider, DraftedCardView, Button } from "../../";
 import { useState, useEffect } from "react";
 
 export const DraftPool = ({ data, draftPoolsState }) => {
@@ -12,11 +12,16 @@ export const DraftPool = ({ data, draftPoolsState }) => {
     Red: "R",
     Green: "G",
   };
+  const [colorIds, setColorIds] = useState(
+    data.colors.picked_multicolor_color_ids,
+  );
   const [unfilteredCards, setUnfilteredCards] = useState(
     data.cards.drafted_cards.filter(
       (card) => card.draft_pool === draftPools[draftPoolsState],
     ),
   );
+
+  const colorIdSet = data.colors.picked_multicolor_color_ids;
 
   const getMaxMV = (cards) => Math.max(...cards.map((card) => card.mv), 0);
 
@@ -30,11 +35,22 @@ export const DraftPool = ({ data, draftPoolsState }) => {
     getMaxMV(unfilteredCards),
   );
 
+  const onClickColorId = (color) => {
+    if (colorIds.includes(color)) {
+      setColorIds(colorIds.filter((id) => id !== color));
+    } else {
+      setColorIds(colorIds.concat(color));
+    }
+  };
+
   useEffect(() => {
     const newState = draftPoolsState;
     const newUnfilteredCards = data.cards.drafted_cards.filter((card) =>
       draftPools[newState].includes(card.draft_pool),
     );
+    if (newState === "Multicolor") {
+      setColorIds(colorIdSet);
+    }
     setNameFilter("");
     setOracleFilter("");
     setTypeFilter("");
@@ -61,7 +77,20 @@ export const DraftPool = ({ data, draftPoolsState }) => {
         .includes(typeFilter.toLowerCase());
       const matchesManaValue =
         card.mv >= minManaValue && card.mv <= maxManaValue;
-      return matchesName && matchesOracle && matchesType && matchesManaValue;
+      switch (draftPoolsState) {
+        case "Multicolor":
+          return (
+            matchesName &&
+            matchesOracle &&
+            matchesType &&
+            matchesManaValue &&
+            colorIds.includes(card.color_identity)
+          );
+        default:
+          return (
+            matchesName && matchesOracle && matchesType && matchesManaValue
+          );
+      }
     });
     setCards(filteredCards);
   }, [
@@ -71,6 +100,7 @@ export const DraftPool = ({ data, draftPoolsState }) => {
     typeFilter,
     minManaValue,
     maxManaValue,
+    colorIds,
   ]);
 
   return (
@@ -80,6 +110,19 @@ export const DraftPool = ({ data, draftPoolsState }) => {
       </h1>
       {data && (
         <div className="w-full max-w-4xl p-6 bg-white shadow-md rounded-lg mt-6">
+          {draftPoolsState === "Multicolor" && (
+            <div className="flex flex-wrap gap-2">
+              {colorIdSet?.map((color) => (
+                <Button
+                  key={color}
+                  title={color}
+                  onClick={() => onClickColorId(color)}
+                  modeType={colorIds}
+                  modeTarget={color}
+                />
+              ))}
+            </div>
+          )}
           <div className="flex flex-row gap-4">
             <TextFilter
               name="Name"
