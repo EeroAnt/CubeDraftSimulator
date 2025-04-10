05c8c1e192b918576b621ec174bcdc8025b00748
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-10 09:23:17 +0300

    add Home view

---
+++ CubeStats/CubeStatsFront/src/components/Views/Home/Home.jsx
@@ -0,0 +1,18 @@
+export const Home = () => {
+  return (
+    <>
+      <h1>Welcome to CubeStats!</h1>
+      <p>
+        This is a tool for analyzing and visualizing your Magic: The Gathering
+        cube data.
+      </p>
+      <p>
+        Use the navigation bar to explore different views and insights about
+        your cube.
+      </p>
+      <p>
+        If you have any questions or feedback, feel free to reach out to us!
+      </p>
+    </>
+  );
+}
