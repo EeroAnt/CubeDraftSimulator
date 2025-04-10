5f29c9c30260cb5e3714c142413d540e4368becc
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-10 15:41:10 +0300

    add first pieces for acks in messages

--- Server/service/Messaging.js
+++ Server/service/Messaging.js
@@ -1,8 +1,12 @@
-import { connections, messageQueues } from "./State.js";
+import { connections, messageQueues, last_acked_message } from "./State.js";
 import { encrypt } from "./encryption.js";
+import { v4 as uuidv4 } from 'uuid';
 
 export const queueMessage = (uuid, message) => {
-  const encryptedMessage = encrypt(JSON.stringify(message));
+  console.log("Message:", message);
+  const messageToEncrypt = addToken(message);
+  console.log("Message with token:", messageToEncrypt);
+  const encryptedMessage = encrypt(JSON.stringify(messageToEncrypt));
   messageQueues[uuid].push(encryptedMessage);
 };
 
@@ -10,4 +14,13 @@ export const processMessageQueue = (uuid) => {
   if (messageQueues[uuid].length > 0) {
     connections[uuid].send(messageQueues[uuid].shift());
   }
+};
+
+const addToken = (message) => {
+  const token = uuidv4();
+  const messageWithToken = {
+    ...message,
+    ackToken: token,
+  };
+  return messageWithToken;
 };
--- Server/service/State.js
+++ Server/service/State.js
@@ -1,5 +1,7 @@
-export const connections = { };
-export const users = { };
-export const drafts = { };
-export const intervalIDs = { };
-export const messageQueues = { };
+export const connections = {};
+export const users = {};
+export const drafts = {};
+export const intervalIDs = {};
+export const messageQueues = {};
+export const last_acked_message = {};
+export const last_updated_draftstate = {};
