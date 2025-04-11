b9c4f8ec26ca492e33c03e2c462adfbadd04fcc1
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-12 00:04:11 +0300

    set states according to correct reconnection

--- Simulator/src/Services/Reconnect.jsx
+++ Simulator/src/Services/Reconnect.jsx
@@ -1,6 +1,9 @@
-export async function reconnect(username, token, seatToken) {
+export async function reconnect(username, token, seatToken, setUsername, setToken, setSeatToken) {
   if (username && token && seatToken) {
     console.log("Reconnecting to draft with seat token: ", seatToken);
+    setUsername(username);
+    setToken(token);
+    setSeatToken(seatToken);
     const message = {
       type: "Rejoin Draft",
       username: username,
@@ -8,15 +11,18 @@ export async function reconnect(username, token, seatToken) {
       seat: seatToken
     };
     return {message, newMode: "Draft"};
-  } else if (username && token) {
+  } else if (username && token && !seatToken) {
     console.log("Reconnecting to lobby with token: ", token);
+    setUsername(username);
+    setToken(token);
     const message = {
       type: "Rejoin Lobby",
       username: username,
       token: token
     };
     return {message, newMode: "Lobby"};
-  } else if (username) {
+  } else if (username && !token && !seatToken) {
+    setUsername(username);
     console.log("Reconnecting to home with username: ", username);
     const message = {
       type: "Login",
