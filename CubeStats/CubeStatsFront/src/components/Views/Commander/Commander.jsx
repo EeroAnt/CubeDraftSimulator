3d8f22e02620c1a081c7c23db343e6063a1aa6f3
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-21 21:32:05 +0300

    utilize new aggregate data with the new table component

--- CubeStats/CubeStatsFront/src/components/Views/Commander/Commander.jsx
+++ CubeStats/CubeStatsFront/src/components/Views/Commander/Commander.jsx
@@ -1,22 +1,12 @@
-import { DraftedCardView, Button } from "../../";
-import { useState, useEffect } from "react";
+import { DraftedCardView, Button, ColorIdTypeTable } from "../../";
+import { useState } from "react";
 
 export const Commander = ({ data }) => {
-  const [cards, setCards] = useState([]);
   const unfilteredCards = data.cards.drafted_cards
     .concat(data.cards.cards_not_drafted)
     .filter((card) => card.is_commander === "true");
-  const colorIdSet = data.colors.commander_color_ids;
-
-  const [colorIds, setColorIds] = useState(colorIdSet);
-
-  useEffect(() => {
-    const filteredCards = unfilteredCards.filter((card) => {
-      const matchesColorId = colorIds.includes(card.color_identity);
-      return matchesColorId;
-    });
-    setCards(filteredCards);
-  }, [colorIds]);
+  const commanderColorIdSet = data.colors.commander_color_ids;
+  const [colorIds, setColorIds] = useState([]);
 
   const onClickColorId = (color) => {
     if (colorIds.includes(color)) {
@@ -33,6 +23,28 @@ export const Commander = ({ data }) => {
     return filteredCards;
   };
 
+  const getColorIds = (colorId) => {
+    const chars = colorId.split("");
+    const results = new Set();
+
+    const generateSubsets = (path, idx) => {
+      if (path.length > 0) {
+        results.add(path.join(""));
+      }
+      for (let i = idx; i < chars.length; i++) {
+        path.push(chars[i]);
+        generateSubsets(path, i + 1);
+        path.pop();
+      }
+    };
+
+    generateSubsets([], 0);
+
+    return Array.from(results).sort(
+      (a, b) => a.length - b.length || a.localeCompare(b),
+    );
+  };
+
   return (
     <>
       <h1 className="text-4xl font-bold text-blue-600 mt-6">Commanders</h1>
@@ -40,7 +52,7 @@ export const Commander = ({ data }) => {
         <>
           <div className="w-full max-w-4xl p-6 bg-white shadow-md rounded-lg mt-6">
             <div className="flex flex-wrap gap-2">
-              {colorIdSet?.map((color) => (
+              {commanderColorIdSet?.map((color) => (
                 <Button
                   key={color}
                   title={color}
@@ -56,6 +68,10 @@ export const Commander = ({ data }) => {
                 <DraftedCardView
                   cards={filterByColorId(colorId)}
                   sortKey="avg_commander_pick"
+                />
+                <ColorIdTypeTable
+                  colorIds={getColorIds(colorId)}
+                  colorIdAggregates={data.aggregates.color_id_aggregates}
                 />
               </div>
             ))}
