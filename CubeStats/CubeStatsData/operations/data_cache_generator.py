3db557a6be9cf91b3ae7292df141ff576e2fd8d6
Eero Antikainen <eeroantikainen90@gmail.com>
2025-04-21 21:11:15 +0300

    fix typos

--- CubeStats/CubeStatsData/operations/data_cache_generator.py
+++ CubeStats/CubeStatsData/operations/data_cache_generator.py
@@ -1,13 +1,13 @@
 from operations.json_writer import write_json
 from operations.card_data import generate_cards_object
 from operations.temp_tables_creator import create_temp_tables
-from operations.aggragates import get_aggregates
+from operations.aggregates import get_aggregates
 from operations.colors import get_colors
 
 def generateDataCache(cursor):
   data = {}
   create_temp_tables(cursor)
-  data["aggragates"] = get_aggregates(cursor)
+  data["aggregates"] = get_aggregates(cursor)
   data["cards"] = generate_cards_object(cursor)
   data["colors"] = get_colors(cursor)
   write_json(data)
