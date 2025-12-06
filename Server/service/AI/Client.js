b1d6c1d08173248f4a0c4c22f16b9d21e5e90552
Eero Antikainen <eeroantikainen90@gmail.com>
2025-12-06 19:40:01 +0200

    use multiple locations for more virtual rate limit

--- Server/service/AI/Client.js
+++ Server/service/AI/Client.js
@@ -1,9 +1,38 @@
 import { AzureOpenAI } from "openai";
+import { nextClientIndex, clientIndex } from "../State.js";
+
 
 export function getClient() {
+  const CLIENTS = [
+    {
+      endpoint: process.env.ENDPOINT_FRANCE,
+      apiKey: process.env.KEY_FRANCE
+    },
+    {
+      endpoint: process.env.ENDPOINT_GERMANY,
+      apiKey: process.env.KEY_GERMANY
+    },
+    {
+      endpoint: process.env.ENDPOINT_ITALY,
+      apiKey: process.env.KEY_ITALY
+    },
+    {
+      endpoint: process.env.ENDPOINT_POLAND,
+      apiKey: process.env.KEY_POLAND
+    },
+    {
+      endpoint: process.env.ENDPOINT_SWEDEN,
+      apiKey: process.env.KEY_SWEDEN
+    },
+  ]
+  const cfg = CLIENTS[clientIndex];
+
+  // move to next for next call
+  nextClientIndex(CLIENTS.length);
+
   return new AzureOpenAI({
-    endpoint: process.env.AZURE_API_ENDPOINT,
-    apiKey: process.env.AZURE_API_KEY,
-    apiVersion: "2025-03-01-preview"
-  });;
+    endpoint: cfg.endpoint,
+    apiKey: cfg.apiKey,
+    apiVersion: "2025-03-01-preview",
+  });
 }
