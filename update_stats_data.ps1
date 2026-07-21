53e8d7ff08c96bd5334ee55cf498df468364b89c
Eero Antikainen <eeroantikainen90@gmail.com>
2026-07-21 12:42:40 +0300

    update scripts for stats page data updates

--- update_stats_data.ps1
+++ update_stats_data.ps1
@@ -2,3 +2,7 @@ cd ./Cubestats/CubestatsData/
 python3 main.py
 cd ../..
 Write-Host "Data successfully updated!"
+git add .\CubeStats\CubeStatsData\data.json
+git commit -m "update data"
+git push
+Write-Host "Data successfully pushed to GitHub!"
